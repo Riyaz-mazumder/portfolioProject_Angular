38f4bbfd3038992d0b02549094e54462d5cddfc5
Riaj <riajmazumder711@gmail.com>
2023-05-18 13:34:35 +0600

    adding full project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './frontEnd/Pages/home-page/home-page.component';
+import { AboutPageComponent } from './frontEnd/Pages/about-page/about-page.component';
+import { ContactPageComponent } from './frontEnd/Pages/contact-page/contact-page.component';
+import { ProjectPageComponent } from './frontEnd/Pages/project-page/project-page.component';
+
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'home', component: HomePageComponent},
+  {path: 'aboutMe', component: AboutPageComponent},
+  {path: 'contactMe', component: ContactPageComponent},
+  {path: 'projects', component: ProjectPageComponent},
+  {path: '**', component: HomePageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { PagesComponent } from './frontEnd/pages/pages.component';
+import { HomePageComponent } from './frontEnd/Pages/home-page/home-page.component';
+import { ProjectPageComponent } from './frontEnd/Pages/project-page/project-page.component';
+import { ContactPageComponent } from './frontEnd/Pages/contact-page/contact-page.component';
+import { AboutPageComponent } from './frontEnd/Pages/about-page/about-page.component';
+import { HeaderComponent } from './frontEnd/Pages/header/header.component';
+import { FooterComponent } from './frontEnd/Pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    PagesComponent,
+    HomePageComponent,
+    ProjectPageComponent,
+    ContactPageComponent,
+    AboutPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
